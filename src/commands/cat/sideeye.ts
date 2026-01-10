140866bdcca78093322c1fdb9b8fff0d1486ba94
Clove Twilight <clovetwilight3@outlook.com>
2026-01-10 06:09:36 +0000

    feat(cat): add sideeye

---
+++ src/commands/cat/sideeye.ts
@@ -0,0 +1,28 @@
+import {
+    ChatInputCommandInteraction,
+    SlashCommandSubcommandBuilder
+} from 'discord.js';
+
+const CHOMP = 'https://tenor.com/view/cat-stare-catstare-cat-stare-sus-catglare-cat-glare-gif-14942558849944709546';
+
+export default {
+    data: new SlashCommandSubcommandBuilder()
+        .setName('sideeye')
+        .setDescription('That\'s sus...')
+        .addUserOption(option =>
+            option
+                .setName('user')
+                .setDescription('Target User')
+                .setRequired(false)
+        ),
+
+    async execute(interaction: ChatInputCommandInteraction) {
+        const targetUser = interaction.options.getUser('user');
+
+        if (targetUser) {
+            await interaction.reply(`${targetUser}[.](${CHOMP})`);
+        } else {
+            await interaction.reply(CHOMP);
+        }
+    }
+};
